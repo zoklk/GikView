ddf35a3c4e1c492153814840a5bef37b1f849279
zoklk <bjincheol34@gmail.com>
2026-06-09 07:07:04 +0000

    fix: 메인페이지 모바일 스크롤 제거 및 파비콘 변경
    Signed-off-by: zoklk <bjincheol34@gmail.com>

--- web/frontend/vite.config.ts
+++ web/frontend/vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc'; // 🚨 패키지명 수정 완료
+import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
 });
